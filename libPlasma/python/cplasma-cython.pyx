9f16788f2710963406b8f2c9022d806c5ea87ec3
Brygg Ullmer <ullmer@acm.org>
2024-07-13 13:20:54 -0400

    incomplete, but iterating forward

---
+++ libPlasma/python/cplasma-cython.pyx
@@ -0,0 +1,17 @@
+// Adaptation by Brygg Ullmer, Clemson University
+// reduction to cython wrapper, initially re JShrake helloWorld example from Animist discord #plasma 
+
+cimport cplasma-cython
+
+  static slaw    extract_slaw (char *arg)
+  pool_cmd_info  plasmaInit(char *pnstr)
+  int            plasmaDeposit(pool_cmd_info cmd, char *descripStr, char ingestStr)
+
+
+  cdef class Queue:
+  cdef cqueue.Queue* _c_queue
+
+  def __cinit__(self):
+    self._c_queue = cqueue.queue_new()
+
+/// end ///
