485c2362c237bd6cfeafdf1930298cf8b1c177e4
Brygg Ullmer <ullmer@acm.org>
2024-07-13 11:04:31 -0400

    beginning to populate

--- libPlasma/python/cplasma.py
+++ libPlasma/python/cplasma.py
@@ -5,14 +5,21 @@
 #cplasma-cython.h
 
 class CPlasma:
-    poolSpecifier = None
+  poolSpecifier = None
 
+  ############# error reporting #############
+
+  def err(self, msg): print("CPlasma error:", msg)
 
   ############# constructor #############
 
   def __init__(self, **kwargs):
-     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
-     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
+    if poolSpecifier is None: err("constructor: poolSpecifier not provided"); return -1
+     
+    self.initPlasma(self.poolSpecifier)
   
   ############# initiate C Plasma #############
 
@@ -20,7 +27,7 @@ class CPlasma:
 
   ############# initiate C Plasma #############
 
-  def deposit(self, a=None, b=None): pass
+  def deposit(self, descrips=None, ingests=None): pass
 
 ################################
 ############# main #############
