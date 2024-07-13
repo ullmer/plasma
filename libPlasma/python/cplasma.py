f9dd5aff407770dc49ef27831c8f34742823eba0
Brygg Ullmer <ullmer@acm.org>
2024-07-13 11:06:30 -0400

    beginning to populate

--- libPlasma/python/cplasma.py
+++ libPlasma/python/cplasma.py
@@ -9,7 +9,8 @@ class CPlasma:
 
   ############# error reporting #############
 
-  def err(self, msg): print("CPlasma error:", msg)
+  def err(self, msg):       print("CPlasma error:", msg)          #to allow for later non-stdout error redirection
+  def msgUpdate(self, msg): print("CPlasma message update:", msg) #to allow for later non-stdout error redirection
 
   ############# constructor #############
 
@@ -17,13 +18,14 @@ class CPlasma:
     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
 
-    if poolSpecifier is None: err("constructor: poolSpecifier not provided"); return -1
+    if poolSpecifier is None: self.err("constructor: poolSpecifier not provided"); return -1
      
     self.initPlasma(self.poolSpecifier)
   
   ############# initiate C Plasma #############
 
-  def initPlasma(self, poolSpecifier=None): pass
+  def initPlasma(self, poolSpecifier=None): 
+    self.msgUpdate("plasma initiating, pool specifier:", poolSpecifier)
 
   ############# initiate C Plasma #############
 
