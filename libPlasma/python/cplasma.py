012ffc78805bbeab4c8289ed53eb29998c1c5e65
Brygg Ullmer <ullmer@acm.org>
2024-07-13 11:01:31 -0400

    beginning to populate

--- libPlasma/python/cplasma.py
+++ libPlasma/python/cplasma.py
@@ -5,11 +5,28 @@
 #cplasma-cython.h
 
 class CPlasma:
+    poolSpecifier = None
+
 
   ############# constructor #############
 
   def __init__(self, **kwargs):
      #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
      self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+  
+  ############# initiate C Plasma #############
+
+  def initPlasma(self, poolSpecifier=None): pass
+
+  ############# initiate C Plasma #############
+
+  def deposit(self, a=None, b=None): pass
+
+################################
+############# main #############
+
+if __name__ == "__main__":
+  p = CPlasma("tcp://localhost/hello")
+  p.deposit("hello", "name:world")
 
 ### end ###
