7b721a576748957eef5a8efcdcf7de41dc22bb43
Brygg Ullmer <ullmer@acm.org>
2024-07-14 11:00:34 -0400

    progress

--- libPlasma/python/cplasma_cython.pyx
+++ libPlasma/python/cplasma_cython.pyx
@@ -4,9 +4,9 @@
 cimport cplasma_cython
 
 cdef class cplasma:
-  slaw           extract_slaw (char *arg)
-  pool_cmd_info  plasmaInit   (char *pnstr)
-  int            plasmaDeposit(pool_cmd_info cmd, char *descripStr, char ingestStr)
+  cdef slaw           extract_slaw (char *arg)
+  cdef pool_cmd_info  plasmaInit   (char *pnstr)
+  cdef int            plasmaDeposit(pool_cmd_info cmd, char *descripStr, char ingestStr)
 
   #def __cinit__(self):
   #  self._c_queue = cqueue.queue_new()
