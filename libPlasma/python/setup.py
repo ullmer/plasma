c2e664e49a4b11e1934c4a756f293352b2afa082
Brygg Ullmer <ullmer@acm.org>
2024-07-14 16:23:38 -0400

    adding libLoam

--- libPlasma/python/setup.py
+++ libPlasma/python/setup.py
@@ -3,8 +3,10 @@ from Cython.Build import cythonize
  
 sourcefiles = ["cplasma.pyx", "cplasmaWrap.c"]
 extensions  = [Extension("cplasma", sourcefiles,
-  libraries    = ["Plasma"],
-  library_dirs = ["/home/bullmer/git/plasma/build/libPlasma/c"])]
+  libraries    = ["Plasma", "Loam"],
+  library_dirs = ["/home/bullmer/git/plasma/build/libPlasma/c",
+                  "/home/bullmer/git/plasma/build/libLoam/c",
+                 ])]
 
 setup(
   ext_modules = cythonize(extensions, build_dir="build")
