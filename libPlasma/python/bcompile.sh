9b8e023acf22fbe31913b434298237efe2fcfca5
Brygg Ullmer <ullmer@acm.org>
2024-07-14 12:48:57 -0400

    renaming

--- libPlasma/python/bcompile.sh
+++ libPlasma/python/bcompile.sh
@@ -1,3 +1,3 @@
 PLASMA_HOME=/home/bullmer/git/plasma
 
-gcc -c -I$PLASMA_HOME/libPlasma/c/ -I$PLASMA_HOME -DABS_TOP_SRCDIR=\"$PLASMA_HOME\" -g cplasma_cython.c 
+gcc -c -I$PLASMA_HOME/libPlasma/c/ -I$PLASMA_HOME -DABS_TOP_SRCDIR=\"$PLASMA_HOME\" -g cplasma.c 
