f64918c498276c83d5fedc7c32ffcf09b84f55be
Brygg Ullmer <ullmer@acm.org>
2024-07-14 10:28:25 -0400

    successfully tested on Win10 WSL2 and Raspberry Pi (5)

--- libPlasma/python/tests/bcompile.sh
+++ libPlasma/python/tests/bcompile.sh
@@ -1,6 +1,8 @@
 PLASMA_HOME=/home/ullmer/git/plasma
+
 WSL2_LIB=/usr/lib/x86_64-linux-gnu
-RPI_LIB=/usr/lib/aarch4-linux-gnu
+RPI_LIB=/usr/lib/aarch64-linux-gnu
+OTHER_LIB=$RPI_LIB
 
 gcc -I.. -I$PLASMA_HOME/libPlasma/c/ -I$PLASMA_HOME \
     -DABS_TOP_SRCDIR=\"$PLASMA_HOME\" -g cpc_test01.c ../*.o -lc \
@@ -9,10 +11,7 @@ $PLASMA_HOME/build/libLoam/c/libLoam.a \
 -L$PLASMA_HOME/build \
 -o cpc_test01 \
 -ldl -lm  -lpthread  -L/lib  -licuuc -licui18n  -lavahi-common -lavahi-client \
-/usr/lib/x86_64-linux-gnu/libyaml.a \
-/usr/lib/x86_64-linux-gnu/libboost_filesystem.a \
-/usr/lib/x86_64-linux-gnu/libboost_regex.a \
-/usr/lib/x86_64-linux-gnu/libboost_system.a \
+$OTHER_LIB/libyaml.a \
+$OTHER_LIB/libboost_filesystem.a $OTHER_LIB/libboost_regex.a $OTHER_LIB/libboost_system.a \
 -L/usr/local/ssl/lib -lssl -lcrypto -lrt
 
-/usr/lib/aarch64-linux-gnu/
