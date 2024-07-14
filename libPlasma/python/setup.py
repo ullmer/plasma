68bc3858bd2ec0d9103427a44402b95facce2efe
Brygg Ullmer <ullmer@acm.org>
2024-07-14 16:44:43 -0400

    fleshing out library dependencies

--- libPlasma/python/setup.py
+++ libPlasma/python/setup.py
@@ -5,15 +5,14 @@ PLASMA_HOME = "/home/bullmer/git/plasma"
 WSL2_LIBD   = "/usr/lib/x86_64-linux-gnu"
 RPI_LIBD    = "/usr/lib/aarch64-linux-gnu"
 
-#OTHER_LIB  = RPI_LIB
-OTHER_LIB   = WSL2_LIB
+#OTHER_LIB  = RPI_LIBD
+OTHER_LIB   = WSL2_LIBD
 
 SSL_LIBD    = "/usr/local/lib"
  
 sourcefiles = ["cplasma.pyx", "cplasmaWrap.c"]
 extensions  = [Extension("cplasma", sourcefiles,
-  libraries    = ["Plasma", "Loam", "ssl", "crypto", "yaml", \
-                  "boost_filesystem", "boost_regex", "boost_system"]
+  libraries = ["Plasma", "Loam", "ssl", "crypto", "yaml", "boost_filesystem", "boost_regex", "boost_system"],
   library_dirs = [PLASMA_HOME + "build/libPlasma/c", \
                   PLASMA_HOME + "build/libLoam/c", \
                   SSL_LIBD, OTHER_LIB \
