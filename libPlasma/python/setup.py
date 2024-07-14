c28a63cdf3fbf1bb7da98fd0269cda02565d820c
Brygg Ullmer <ullmer@acm.org>
2024-07-14 15:24:23 -0400

    progress

--- libPlasma/python/setup.py
+++ libPlasma/python/setup.py
@@ -2,11 +2,12 @@ from setuptools   import Extension, setup
 from Cython.Build import cythonize
  
 sourcefiles = ["cplasma.pyx", "cplasmaWrap.c"]
-extensions  = [Extension("cplasma", sourcefiles)]
+extensions  = [Extension("cplasma", sourcefiles,
+  libraries    = ["libPlasma"],
+  library_dirs = ["/home/bullmer/git/plasma/build/libPlasma/c/"])]
 
 setup(
-  ext_modules = cythonize(extensions, build_dir="build"),
-  libraries   = ["libPlasma"],
+  ext_modules = cythonize(extensions, build_dir="build")
 )
 
 ### end ###
