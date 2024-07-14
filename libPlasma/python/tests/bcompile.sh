ffd796e5395b82b4876a34b4d5a861f9e944b73f
Brygg Ullmer <ullmer@acm.org>
2024-07-14 10:06:00 -0400

    working toward rpi compatibility

--- libPlasma/python/tests/bcompile.sh
+++ libPlasma/python/tests/bcompile.sh
@@ -1,4 +1,6 @@
-PLASMA_HOME=/home/bullmer/git/plasma
+PLASMA_HOME=/home/ullmer/git/plasma
+WSL2_LIB=/usr/lib/x86_64-linux-gnu
+RPI_LIB=/usr/lib/aarch4-linux-gnu
 
 gcc -I.. -I$PLASMA_HOME/libPlasma/c/ -I$PLASMA_HOME \
     -DABS_TOP_SRCDIR=\"$PLASMA_HOME\" -g cpc_test01.c ../*.o -lc \
@@ -12,3 +14,5 @@ $PLASMA_HOME/build/libLoam/c/libLoam.a \
 /usr/lib/x86_64-linux-gnu/libboost_regex.a \
 /usr/lib/x86_64-linux-gnu/libboost_system.a \
 -L/usr/local/ssl/lib -lssl -lcrypto -lrt
+
+/usr/lib/aarch64-linux-gnu/
