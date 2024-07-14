e0c379ad76b9e9048057bb600274dd2cc680129b
Brygg Ullmer <ullmer@acm.org>
2024-07-14 12:47:58 -0400

    renaming

--- libPlasma/python/cplasma.pyx
+++ libPlasma/python/cplasma.pyx
@@ -3,9 +3,11 @@
 
 cimport cplasma_cython
 
-cdef slaw           extract_slaw (char *arg)
-cdef pool_cmd_info  plasmaInit   (char *pnstr)
-cdef int            plasmaDeposit(pool_cmd_info cmd, char *descripStr, char ingestStr)
+print("hello foo")
+
+#cdef slaw           extract_slaw (char *arg)
+#cdef pool_cmd_info  plasmaInit   (char *pnstr)
+#cdef int            plasmaDeposit(pool_cmd_info cmd, char *descripStr, char ingestStr)
 
 #def __cinit__(self):
 #  self._c_queue = cqueue.queue_new()
