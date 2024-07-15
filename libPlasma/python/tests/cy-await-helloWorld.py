d018e0aefc78290f5355b55ec1f5c203343a4993
Brygg Ullmer <ullmer@acm.org>
2024-07-15 13:05:12 -0400

    renaming to pAwait and pDeposit, given "await" as a python keyword

--- libPlasma/python/tests/cy-await-helloWorld.py
+++ libPlasma/python/tests/cy-await-helloWorld.py
@@ -2,5 +2,5 @@ import sys; sys.path.append("/home/ullmer/git/plasma/libPlasma/python/")
 import cplasma
 
 cplasma.init("tcp://localhost/hello")
-cplasma.pDeposit("hello", "name:world")
+cplasma.pAwait()
 
