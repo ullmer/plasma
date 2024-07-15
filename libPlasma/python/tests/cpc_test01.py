51060863f539216c0047789dd33010f7e0de673f
Brygg Ullmer <ullmer@acm.org>
2024-07-15 11:36:12 -0400

    python hello-world deposit

--- libPlasma/python/tests/cpc_test01.py
+++ libPlasma/python/tests/cpc_test01.py
@@ -1,6 +1,7 @@
+import sys
+sys.path.append("/home/ullmer/git/plasma/libPlasma/python/")
 import cplasma
 
-p = cplasma.CPlasma("tcp://localhost/hello")
-p.deposit("hello", "name:world")
+cplasma.init("tcp://localhost/hello")
+cplasma.deposit("hello", "name:world")
 
-### end ###
