64d2c24b02629b366319b99377b69c3e988266f7
Brygg Ullmer <ullmer@acm.org>
2024-07-15 10:26:57 -0400

    progress on link flags

--- libPlasma/python/setup.py
+++ libPlasma/python/setup.py
@@ -1,8 +1,8 @@
 from setuptools   import Extension, setup
 from Cython.Build import cythonize
 
-#PLASMA_HOME = "/home/bullmer/git/plasma/"
-PLASMA_HOME = "/home/ullmer/git/plasma/"
+PLASMA_HOME = "/home/bullmer/git/plasma/"
+#PLASMA_HOME = "/home/ullmer/git/plasma/"
 WSL2_LIBD   = "/usr/lib/x86_64-linux-gnu"
 RPI_LIBD    = "/usr/lib/aarch64-linux-gnu"
 
@@ -16,8 +16,12 @@ extensions  = [Extension("cplasma", sourcefiles,
   libraries = ["Plasma", "Loam", "ssl", "crypto", "yaml", "boost_filesystem", "boost_regex", "boost_system"],
   library_dirs = [PLASMA_HOME + "build/libPlasma/c", \
                   PLASMA_HOME + "build/libLoam/c", \
-                  SSL_LIBD, OTHER_LIB \
-                 ])]
+                  SSL_LIBD, OTHER_LIB],
+  extra_compile_args = ["-fPIC"],
+  extra_link_args    = ["-fPIC"],
+  compile_args = ["-fPIC"],
+  link_args    = ["-fPIC"]
+                 )]
 
 setup(
   ext_modules = cythonize(extensions, build_dir="build")
