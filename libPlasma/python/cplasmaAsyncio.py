f59ad38bc752fa1ab99a99950b547093882e6bd8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-16 00:19:02 -0400

    progress

--- libPlasma/python/cplasmaAsyncio.py
+++ libPlasma/python/cplasmaAsyncio.py
@@ -1,4 +1,31 @@
 #https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.run_in_executor
 
+import asyncio
+import concurrent.futures
+
+def blocking_io():
+  # File operations (such as logging) can block the
+  # event loop: run them in a thread pool.
+  with open('/dev/urandom', 'rb') as f:
+    return f.read(100)
+
+async def main():
+  loop = asyncio.get_running_loop()
+
+  ## Options:
+  # 2. Run in a custom thread pool:
+  with concurrent.futures.ThreadPoolExecutor() as pool:
+    result = await loop.run_in_executor(
+      pool, blocking_io)
+    print('custom thread pool', result)
+
+  # 3. Run in a custom process pool:
+  #with concurrent.futures.ProcessPoolExecutor() as pool:
+  #  result = await loop.run_in_executor(
+  #    pool, cpu_bound)
+  #  print('custom process pool', result)
+
+if __name__ == '__main__':
+  asyncio.run(main())
 
 ### end ###
