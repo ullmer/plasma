188281f14e69c4846f84479d5ff3627da01f3a1c
Brygg Ullmer <ullmer@acm.org>
2024-07-16 17:36:41 -0400

    asyncio seems operable

--- libPlasma/python/tests/cy-await-helloWorld04.py
+++ libPlasma/python/tests/cy-await-helloWorld04.py
@@ -3,23 +3,20 @@ import cplasma
 import asyncio, sys, time
 
 cplasma.init("tcp://localhost/hello")
-time.sleep(4)
-#fmtStr        = cplasma.getProtFormatStr('prot:simpleKeyVal')
-fmtStr = "{D:[S],I:{S: S}}"
-
-print("watching cplasma for updates of form", fmtStr)
+fmtStr        = cplasma.getProtFormatStr('prot:simpleKeyVal')
 sleepDuration = .01
 
 async def plasmaWatcher():
   global sleepDuration, fmtStr
 
-  try:    strs = cplasma.pNext(fmtStr)
-  except: print("plasmaWatcher: pNext error!"); return
+  while True:
+    try:    strs = cplasma.pNext(fmtStr)
+    except: print("plasmaWatcher: pNext error!"); return
 
-  if strs is None: await asyncio.sleep(sleepDuration)
-  else: print("<<%s>>" % strs)
+    if strs is None: await asyncio.sleep(sleepDuration)
+    else: print("<<%s>>" % strs)
 
-#asyncio.run(plasmaWatcher())
+asyncio.run(plasmaWatcher())
 
 cplasma.close()
 
