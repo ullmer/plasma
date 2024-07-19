ffd5678d13831e4f1d025cf7373dfadb2b40ca27
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-19 00:44:26 -0400

    iterating on syntax compliance

--- libPlasma/c/tuio_bundles.h
+++ libPlasma/c/tuio_bundles.h
@@ -4,13 +4,13 @@
 
 #define int12 _BitInt(12)
 
-typedef struct tuio2_ptr_12bit { //https://en.cppreference.com/w/c/language/union ; incl. anonymous union
+typedef struct { //https://en.cppreference.com/w/c/language/union ; incl. anonymous union
   union { char data[8];
           struct {int12 s_id, tu_id, c_id, 
                         x_pos,  y_pos,
                         angle,  shear, 
                         radius, press;};
-        };
+        } tuio2_ptr_12bit ;
 };
 
 /// end ///
