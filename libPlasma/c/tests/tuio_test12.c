a659e8312d875529017bac4bf5d2b0c20d77bf37
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-19 00:30:22 -0400

    minor alignment

---
+++ libPlasma/c/tests/tuio_test12.c
@@ -0,0 +1,18 @@
+// Experimental expression of TUIO2-style data structures/bundles toward Plasma mgmt/transport
+// Initiated by Brygg Ullmer, Clemson University
+// Begun 2024-07-19
+
+#include "tuio_bundles."
+
+#define int12 _BitInt(12)
+
+struct tuio2_ptr_12bit { //https://en.cppreference.com/w/c/language/union ; incl. anonymous union
+  union { char data[8];
+          struct {int12 s_id, tu_id, c_id, 
+                        x_pos,  y_pos,
+                        angle,  shear, 
+                        radius, press;}
+        }
+}
+
+/// end ///
