f3c171f3bcf8c3f948cdcd8597bfbb28b0b8a6d7
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-19 14:42:14 -0400

    progress

--- libPlasma/c/tests/tuio_test12.c
+++ libPlasma/c/tests/tuio_test12.c
@@ -2,13 +2,17 @@
 // Initiated by Brygg Ullmer, Clemson University
 // Begun 2024-07-19
 
+#include <stdio.h>
 #include "tuio_bundles.h"
 
 int main() {
   tuio2_ptr_12bit tpb;
-  tpb.xpos = 100;
-  tpb.ypos = 3000;
-  print("%x%x%x%x\n", tpb.data[0], tpb.data[1], tpb.data[2], tpb.data[3]);
+
+  printf("GCC version number: %i\n", __GNUC__);
+
+  //tpb.xpos = 100;
+  //tpb.ypos = 3000;
+  //printf("%x%x%x%x\n", tpb.data[0], tpb.data[1], tpb.data[2], tpb.data[3]);
 }
 
 /// end ///
