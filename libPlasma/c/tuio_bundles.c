47e9d8ae9250aa0509f53fb16df6e9fed3dea209
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-19 15:04:02 -0400

    progress

--- libPlasma/c/tuio_bundles.c
+++ libPlasma/c/tuio_bundles.c
@@ -12,15 +12,29 @@ void plasmaTuio2Ptr_setXY(int X, int Y, tuio2_ptr_12bit *tpb) {
 
 #else
 
+#define MAX_12BIT 4095
+
 void plasmaTuio2Ptr_setXY(int X, int Y, tuio2_ptr_12bit *tpb) {
- // warming up to careful bitshifting and bitsetting
+
+ if (X > #MAX_12BIT || X < 0) {
+   printf("plasmaTuio2Ptr_setXY error: X value not between 0..%i (%i)\n", MAX_12BIT, X); return;
+ }
+
+ if (Y > #MAX_12BIT || X < 0) {
+   printf("plasmaTuio2Ptr_setXY error: Y value not between 0..%i (%i)\n", MAX_12BIT, X); return;
+ }
+
+ // since angle may be assigned, we need to respect it.
+ int posXY    = (X << 24) + (Y << 12); 
+ *tpb.posAng &= MAX_12BIT; // clear out any existing X & Y components
+ *tpb.posAng |= posXY;
+
  // struct {int12 s_id, tu_id, c_id,
  //         x_pos,  y_pos, angle,  ...  tuio2_ptr_12bit;
   
  // typedef union { char data[9];
  //                 struct {char id[3];
  //                         char posAng[3];
-
 }
 #endif
 
