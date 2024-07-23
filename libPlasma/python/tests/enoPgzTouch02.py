c39bedf88dc4345f7b342d16d6512717f9b82b15
Brygg Ullmer <ullmer@acm.org>
2024-07-23 00:33:22 -0400

    progress

--- libPlasma/python/tests/enoPgzTouch02.py
+++ libPlasma/python/tests/enoPgzTouch02.py
@@ -12,8 +12,7 @@ LIB_PATH = pathlib.Path(__file__).parents[1] #library in parent directory
 sys.path.append(os.path.join(LIB_PATH, ''))
 
 from proteinSchemas import *
-
-from enoPgz import *
+from enoPgz         import *
 
 touch_coords = {}
 def normalizePos(x,y): return (int(x*WIDTH), int(y*HEIGHT))
@@ -21,16 +20,18 @@ def normalizePos(x,y): return (int(x*WIDTH), int(y*HEIGHT))
 ############ finger -- potentially multitouch -- events ##########
 
 def on_finger_down(finger_id, x, y):
-  ix,iy = WIDTH*x, HEIGHT*y
+  ix, iy                  = normalizePos(x,y)
+  touch_coords[finger_id] = [ix,iy]
+
+  multitouchDeposit([finger_id, ix, iy])
   print("finger DOWN", finger_id)
-  mtDeposit([finger_id, ix, iy])
-  touch_coords[finger_id] = normalizePos(x,y)
 
 def on_finger_move(finger_id, x, y):
-  ix,iy = WIDTH*x, HEIGHT*y
+  ix, iy                  = normalizePos(x,y)
+  touch_coords[finger_id] = [ix,iy]
+
+  multitouchDeposit([finger_id, ix, iy])
   print("finger MOVE", ix, iy)
-  mtDeposit([finger_id, ix, iy])
-  touch_coords[finger_id] = normalizePos(x,y)
 
 def on_finger_up(finger_id, x, y):
   print("finger UP")
@@ -55,7 +56,7 @@ def draw():
 
 ps = proteinSchemas(schemaIndexPath='/home/ullmer/git/plasma/libPlasma/yaml')
 cplasma.init("tcp://localhost/hello")
-mtDeposit = ps.registerHwSensorDepositor('C2d_generic')
+multitouchDeposit = ps.registerHwSensorDepositor('C2d_generic')
 
 pgzx = enoPgz(["multitouch"])
 pgzx.go()
