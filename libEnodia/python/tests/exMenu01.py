39466561544dd6e6864326ba039f607972fc0e46
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-11 16:23:51 -0400

    progress

--- libEnodia/python/tests/exMenu01.py
+++ libEnodia/python/tests/exMenu01.py
@@ -3,23 +3,14 @@
 
 import sys; sys.path.append("..")
 
-from enoButton import *
+from enoMenu import *
 
 WIDTH, HEIGHT = 900, 900
 bd = (100, 100)
 
+em = enoMenu()
 
-
-b1 = enoButton("animist", imageFn="glyphs/animist01a_100", basePos=(50,  50), drawText=False, buttonDim=bd)
-b2 = enoButton("BOS",     imageFn="glyphs/map_bos01a_100", basePos=(50, 155), drawText=False, buttonDim=bd)
-b3 = enoButton("MIT",     imageFn="glyphs/map_mit01a_100", basePos=(50, 260), drawText=False, buttonDim=bd)
-buttons = [b1,b2,b3]
-
-def draw():             
-  screen.clear()
-  for b in buttons: b.draw(screen)
-
-def on_mouse_down(pos): 
-  for b in buttons: b.on_mouse_down(pos)
+def draw(): screen.clear(); em.draw(screen)
+def on_mouse_down(pos):     em.on_mouse_down(pos)
 
 ### end ###
