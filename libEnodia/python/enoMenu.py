9f5be2dd0e503ab3f45b6c1cc2aab21bb428fca6
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-11 15:28:55 -0400

    progress

--- libEnodia/python/enoMenu.py
+++ libEnodia/python/enoMenu.py
@@ -28,15 +28,18 @@ class enoMenu:
   drawImg    = False
   drawAdapt  = True   # if True, will render text and/or image only when specified
 
+  self.whichMenuName = "homeMenu"
+
   verbose     = False
 
   ############# constructor #############
 
-  def __init__(self, **kwargs): 
+  def __init__(self, whichMenuName=None, **kwargs): 
 
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
 
+    if whichMenuName is not None: self.whichMenuName = whichMenuName
     self.loadYaml()
 
   ############# error message #############
@@ -48,6 +51,14 @@ class enoMenu:
   def loadYaml(self):
     if self.yamlFn is None:             err("loadYaml: yaml filename undefined"); return
     if os.path.exists(yamlFn) == False: err("loadYaml: yaml file not found:" + self.yamlFn); return
+
+    f = open(self.yamlFn, 'rt')
+    self.yamlD = yaml.safe_load(f)
+    f.close()
+
+    if 'animist' not in self.yamlD: err("loadYaml: animist not found in yaml" + self.yamlFn); return
+
+    ad = self.yamlD['animist']
     
 
 ### end ###
