283d36830e0ae4f5099f7c214523107e06a001bd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-10 23:18:57 -0400

    vertical text

---
+++ libEnodia/python/tests/exText03.py
@@ -0,0 +1,35 @@
+# https://pygame-zero.readthedocs.io/en/stable/ptext.html
+# https://pythonprogramming.altervista.org/pygame-4-fonts/
+
+import sys; sys.path.append("..")
+
+from enoButton import *
+import yaml
+
+WIDTH, HEIGHT = 900, 900
+
+panel1Fn = 'yaml/panel1.yaml'
+panel1F  = open(panel1Fn, 'r+t')
+panel1Y  = yaml.safe_load(panel1F)
+
+print(panel1Y)
+
+global panel
+panel = []
+dx = 50; idx = 0
+
+for row in panel1Y: #rows
+  ba = enoButtonArray(row, buttonDim=(30, 150), dx=0, dy=160, basePos=(dx*idx, 0), angle=90)
+  panel.append(ba); idx += 1
+
+rect1   = Rect((130, 5), (200, 200))
+bgcolor = "#aa0000"
+
+def draw(): 
+  for ba in panel: ba.draw(screen)
+  screen.draw.filled_rect(rect1, bgcolor)
+
+def on_mouse_down(pos):
+  for ba in panel: ba.on_mouse_down(pos)
+
+### end ###
