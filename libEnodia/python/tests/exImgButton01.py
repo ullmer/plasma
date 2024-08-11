20493d808e9f64914f45c62900677418cd1fc01e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-11 12:58:12 -0400

    debugging

---
+++ libEnodia/python/tests/exImgButton01.py
@@ -0,0 +1,17 @@
+# https://pygame-zero.readthedocs.io/en/stable/ptext.html
+# https://pythonprogramming.altervista.org/pygame-4-fonts/
+
+import sys; sys.path.append("..")
+
+from enoButton import *
+
+WIDTH, HEIGHT = 900, 900
+
+b1 = enoButton("animist", imgFn="glyphs/animist01a_100", basePos=(50,  50))
+b2 = enoButton("BOS",     imgFn="glyphs/map_bos01a_100", basepos=(50, 125))
+buttons = [b1,b2]
+
+def draw():             for button in buttons: button.draw()
+def on_mouse_down(pos): for button in buttons: button.on_mouse_down(pos)
+
+### end ###
