fbe30eb6cc6af86657b8da35b0b253f54bcf653e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-11 23:14:27 -0400

    about to extent interactivity

---
+++ libEnodia/python/tests/exMenu02.py
@@ -0,0 +1,14 @@
+# https://pygame-zero.readthedocs.io/en/stable/ptext.html
+# https://pythonprogramming.altervista.org/pygame-4-fonts/
+
+import sys; sys.path.append("..")
+from enoMenu import *
+
+WIDTH, HEIGHT = 900, 900
+
+em = enoMenu(yamlFn = "yaml/animistHomeMenu01.yaml", requestAnim=True)
+
+def draw(): screen.clear(); em.draw(screen)
+def on_mouse_down(pos):     em.on_mouse_down(pos)
+
+### end ###
