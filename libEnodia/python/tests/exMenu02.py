4983e82faf1067dc5088dae325d4d3de107080f8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-11 23:15:39 -0400

    progress

--- libEnodia/python/tests/exMenu02.py
+++ libEnodia/python/tests/exMenu02.py
@@ -2,13 +2,13 @@
 # https://pythonprogramming.altervista.org/pygame-4-fonts/
 
 import sys; sys.path.append("..")
-from enoMenu import *
+from enoAnimistHomeMenu import *
 
 WIDTH, HEIGHT = 900, 900
 
-em = enoMenu(yamlFn = "yaml/animistHomeMenu01.yaml", requestAnim=True)
+eahm = enoAnimistHomeMenu(yamlFn = "yaml/animistHomeMenu01.yaml", requestAnim=True)
 
-def draw(): screen.clear(); em.draw(screen)
-def on_mouse_down(pos):     em.on_mouse_down(pos)
+def draw(): screen.clear(); eahm.draw(screen)
+def on_mouse_down(pos):     eahm.on_mouse_down(pos)
 
 ### end ###
