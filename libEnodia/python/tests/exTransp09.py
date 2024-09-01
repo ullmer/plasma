9e74a67a91a1904d926d628515b676fcf9427391
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-31 23:29:22 -0400

    progress

--- libEnodia/python/tests/exTransp09.py
+++ libEnodia/python/tests/exTransp09.py
@@ -9,7 +9,20 @@ WIDTH, HEIGHT = 300, 300
 from enoTranspWinDance import *
 from enoWinMgr         import *
 
-class exTransp09(enoTranspWinDance): pass
+################## example transparency 09 ##################
+
+class exTransp09(enoTranspWinDance): 
+
+  def firstFrame(self):
+    super().firstFrame()
+
+    for winId in [1, 2]:
+      w      = self.winCoordProxies[winId]
+      x1, y1 = w.pos
+      x2, y2 = x1 + 1000, y1
+      animate(w, pos=(x2, y2), tween=self.tween, duration=self.dur1)
+
+################## main ##################
   
 ewm = enoWinMgr()
 et9 = exTransp09(ewm=ewm)
