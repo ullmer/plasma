fb60f99380b14f182128a582c81e64091985a62c
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-31 23:39:15 -0400

    progress

--- libEnodia/python/tests/exTransp09.py
+++ libEnodia/python/tests/exTransp09.py
@@ -9,9 +9,15 @@ WIDTH, HEIGHT = 300, 300
 from enoTranspWinDance import *
 from enoWinMgr         import *
 
+from functools         import partial
+
 ################## example transparency 09 ##################
 
 class exTransp09(enoTranspWinDance): 
+
+  def subwinAnimBounce(self, whichSubwin, nextDest, subsequentDest):
+    cb = partial(self.subwinAnimBounce, whichSubwin, subsequentDest, nextDest)
+    animate(whichSubwin, pos=nextDest, tween=self.tween, duration=self.dur1, on_finished=cb)
 
   def firstFrame(self):
     super().firstFrame()
@@ -19,8 +25,10 @@ class exTransp09(enoTranspWinDance):
     for winId in [1, 2]:
       w      = self.winCoordProxies[winId]
       x1, y1 = w.pos
-      x2, y2 = x1 + 1000, y1
-      animate(w, pos=(x2, y2), tween=self.tween, duration=self.dur1)
+      x2, y2 = x1 + 1700, y1
+
+      cb = partial(self.subwinAnimBounce, w, (x1, y1), (x2, y2))
+      animate(w, pos=(x2, y2), tween=self.tween, duration=self.dur1, on_finished=cb)
 
 ################## main ##################
   
