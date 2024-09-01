3dd97a1023f91494bbb1fc2cf163ac3fb681920f
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-31 21:57:06 -0400

    progress

--- libEnodia/python/tests/exTransp08.py
+++ libEnodia/python/tests/exTransp08.py
@@ -28,7 +28,7 @@ class exTransp08:
   pRenderers      = None
   pWindows        = None
 
-  dur             = 1.5 #duration
+  dur1, dur2      = 1.5, 4. #durations
   fuchsia         = (255, 0, 128)  # Transparency key color
 
   justBeginning   = True
@@ -38,6 +38,7 @@ class exTransp08:
   winDimension    = (250, 250)
   winCoords       = [(0, 0), (0, 300), (0, 600)]
   ewm             = None #enoWinMgr
+  tween           = 'accel_decel'
 
   ############# constructor #############
 
@@ -61,7 +62,7 @@ class exTransp08:
     wh, ww = self.winDimension
     
     pWindows[0] = self.ewm.getWindow()
-    n = self.numSubwins - 1
+    n           = self.numSubwins - 1
 
     for i in [1, n]: 
       # dictionaries work for storing window handle, but lists do not, because of pygame_sdl2's "deep copy" limitations
@@ -71,53 +72,48 @@ class exTransp08:
   
     #pWindows = [window1] #this "deepcopy" is sufficient to cause a segfault; long, long sigh
   
-    for i in range(3):
-      x, y                   = winCoords[i]
-      winCoordProxies[i].pos = winCoords[i]
-      moveWindow(pWindows[i], x, y)
+    for i in range(self.numSubwins):
+      x, y                        = self.winCoords[i]
+      self.winCoordProxies[i].pos = self.winCoords[i]
+      self.ewm.moveWindow(self.pWindows[i], x, y)
   
-    transpWinSetup(screen, fuchsia, WIDTH, HEIGHT)                     #set up transparent window ~chromakey
+    self.ewm.transpWinSetup(screen, fuchsia, WIDTH, HEIGHT)                     #set up transparent window ~chromakey
     #transpWinSetup(pRenderers[1], fuchsia, WIDTH, HEIGHT, pWindows[1]) #set up transparent window ~chromakey
   
-    animate(a1, pos=pos2, tween='accel_decel', duration=dur, on_finished=animTransition1)
+    animate(self.a1, pos=self.pos2, tween=self.tween, duration=self.dur1, on_finished=self.animTransition1)
   
-    w1 = winCoordProxies[0]
-    animate(w1, pos=(800,800), tween='accel_decel', duration=4.)
+    w1 = self.winCoordProxies[0]
+    animate(w1, pos=(800,800), tween=self.tween, duration=self.dur2)
   
   ##################### animation transition #####################
   
-  def animTransition1():
-    global animPhase1
-  
-    if animPhase1:
-      animPhase1 = False; animate(a1, pos=pos1, tween='accel_decel', duration=dur, on_finished=animTransition)
+  def animTransition1(self):
+    if self.animPhase1:
+      self.animPhase1 = False; animate(a1, pos=self.pos1, tween=self.tween, duration=dur1, on_finished=self.animTransition)
     else:
-      animPhase1 = True;  animate(a1, pos=pos2, tween='accel_decel', duration=dur, on_finished=animTransition)
+      self.animPhase1 = True;  animate(a1, pos=self.pos2, tween=self.tween, duration=dur1, on_finished=self.animTransition)
   
   ##################### draw #####################
   
-  def draw(ewm):
-    global pRenderers, pWindows, winCoordProxies
-    global justBeginning
+  def draw(self):
+    if self.justBeginning: self.firstFrame(); self.justBeginning=False
   
-    if justBeginning: firstFrame(); justBeginning=False
+    screen.fill(self.fuchsia)  # Transparent background ~chromakey
+    self.a1.draw()
   
-    screen.fill(fuchsia)  # Transparent background ~chromakey
-    a1.draw()
-  
-    for i in range(3):
-     wp = winCoordProxies[i]
+    for i in range(self.numSubwins):
+     wp = self.winCoordProxies[i]
      x, y = wp.pos
-     moveWindowById(i, x, y, pWindows)
+     self.ewm.moveWindowById(i, x, y, self.pWindows)
   
-    for i in [1,2]:
-      pRenderers[i].clear()
-      pRenderers[i].present()
+    n = self.numSubwins - 1
+    for i in [1,n]:
+      self.pRenderers[i].clear()
+      self.pRenderers[i].present()
   
-  ewm = enoWinMgr()
-
 ##################### main #####################
   
+ewm = enoWinMgr()
 def drawEwm(ewm): ewm.draw()
   
 draw = partial(drawEwm, ewm)
