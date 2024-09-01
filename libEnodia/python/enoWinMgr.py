8f91eb29c2e616bf146d8206c425bf5f3617e642
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-08-31 21:38:42 -0400

    progress

--- libEnodia/python/enoWinMgr.py
+++ libEnodia/python/enoWinMgr.py
@@ -15,7 +15,13 @@ import win32con
 import win32gui
 from pygame._sdl2 import Window
 
+###################### Enodia Window Manager (first steps) ###################### 
+
 class enoWinMgr:
+
+  name2window            = None
+  moveWindowLastCoords   = None
+  moveWindowIdLastCoords = None
 
   ############# constructor #############
 
@@ -24,62 +30,53 @@ class enoWinMgr:
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
 
-    self.name2window = {}
+    self.name2window            = {}
+    self.moveWindowLastCoords   = {} #unsure because of pygame_sdl2 deepcopy issue about aspects here
+    self.moveWindowIdLastCoords = {}
 
   ##################### new window ##################### 
    
   def newWindow(self, name, w,h):
-    global name2window 
-    #print("nw:", str(name2window))
-  
     pWindow           = Window(name, size=(w,h))
-    name2window[name] = pWindow
+    self.name2window[name] = pWindow
     return pWindow
   
   ##################### get window ##################### 
   
   def getWindow(self, name=None):
-    global name2window 
     if name=='firstWin' or name is None: return Window.from_display_module()
-    if name in name2window:              result = name2window[name]; return result
-  
-  moveWindowLastCoords   = {} #unsure because of pygame_sdl2 deepcopy issue about aspects here
-  moveWindowIdLastCoords = {}
+    if name in self.name2window:         result = self.name2window[name]; return result
   
   ##################### move window ##################### 
   
-  def moveWindow(pWindow, x,y):
-    global moveWindowLastCoords
-  
-    if pWindow in moveWindowLastCoords:
-      lastX, lastY = moveWindowLastCoords[pWindow]
+  def moveWindow(self, pWindow, x,y):
+    if pWindow in self.moveWindowLastCoords:
+      lastX, lastY = self.moveWindowLastCoords[pWindow]
       if x == lastX and y == lastY: return #no movement
   
-    else: moveWindowLastCoords[pWindow] = (x,y)
+    else: self.moveWindowLastCoords[pWindow] = (x,y)
   
     if pWindow is None: pWindow=getWindow()
     pWindow.position = (x,y) #titlebar slightly off-screen
   
   ##################### move window by id ##################### 
   
-  def moveWindowById(windowId, x,y, pWindows):
-    global moveWindowIdLastCoords
-  
-    if windowId in moveWindowIdLastCoords:
-      lastX, lastY = moveWindowIdLastCoords[windowId]
+  def moveWindowById(self, windowId, x,y):
+    if windowId in self.moveWindowIdLastCoords:
+      lastX, lastY = self.moveWindowIdLastCoords[windowId]
       if x == lastX and y == lastY: return #no movement
   
-    else: moveWindowIdLastCoords[windowId] = (x,y)
+    else: self.moveWindowIdLastCoords[windowId] = (x,y)
   
-    if windowId not in pWindows:
+    if windowId not in self.pWindows:
       print("moveWindowById error: id index not found:", windowId); return
   
-    pWindow = pWindows[windowId]
+    pWindow = self.pWindows[windowId]
     pWindow.position = (x,y)
   
   ##################### transparent window setup ##################### 
   
-  def transpWinSetup(screen, keyColor, winWidth, winHeight, window=None):
+  def transpWinSetup(self, screen, keyColor, winWidth, winHeight, window=None):
     imgIcon = pygame.image.load("images/animist01e.png")
     pygame.display.set_icon(imgIcon)
     pygame.display.set_caption("animist alpha")
