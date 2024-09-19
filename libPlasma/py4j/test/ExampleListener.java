78f81042fbbeb96f05a55b8b6ada8afda02c1778
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-19 18:07:13 -0400

    callback test from py4j documentation

---
+++ libPlasma/py4j/test/ExampleListener.java
@@ -0,0 +1,11 @@
+// https://www.py4j.org/advanced_topics.html#implementing-java-interfaces-from-python-callback
+
+// py4j/examples/ExampleListener.java
+
+package py4j.examples;
+
+public interface ExampleListener {
+  Object notify(Object source);
+}
+
+/// end ///
---
+++ libPlasma/py4j/test/ListenerApplication.java
@@ -0,0 +1,38 @@
+//https://www.py4j.org/advanced_topics.html#implementing-java-interfaces-from-python-callback
+
+// py4j/examples/ListenerApplication.java
+package py4j.examples;
+
+import py4j.GatewayServer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListenerApplication {
+
+    List<ExampleListener> listeners = new ArrayList<ExampleListener>();
+
+    public void registerListener(ExampleListener listener) {
+        listeners.add(listener);
+    }
+
+    public void notifyAllListeners() {
+        for (ExampleListener listener: listeners) {
+            Object returnValue = listener.notify(this);
+            System.out.println(returnValue);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "<ListenerApplication> instance";
+    }
+
+    public static void main(String[] args) {
+        ListenerApplication application = new ListenerApplication();
+        GatewayServer server = new GatewayServer(application);
+        server.start(true);
+    }
+}
+
+/// end ///
