a75c7670e39a82c28d06cff9c013226d9ef27fa8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-22 13:32:04 -0400

    progress

---
+++ libPlasma/py4j/test/exP4j03.py
@@ -0,0 +1,6 @@
+from py4j.java_gateway import JavaGateway, GatewayParameters
+
+gwparam = GatewayParameters(address='127.0.0.1', port=25333)
+gw = JavaGateway(gateway_parameters=gwparam)
+rc = gw.entry_point
+
