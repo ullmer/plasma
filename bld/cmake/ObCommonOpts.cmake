d02cf96e8774390c933f49d11fb04c0dca2372a5
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-05-25 23:18:25 -0400

    gspeak to plasma

--- bld/cmake/ObCommonOpts.cmake
+++ bld/cmake/ObCommonOpts.cmake
@@ -7,7 +7,7 @@
 # ObCommonOpts
 # ------------
 #
-# Boilerplate cmake options for projects using g-speak
+# Boilerplate cmake options for projects using plasma
 #
 # Defines cmake commandline options settable with -Dfoo=bar:
 #   ASAN                   - whether to use address sanitizer
@@ -16,13 +16,13 @@
 #   CMAKE_VERBOSE_MAKEFILE - whether build should be verbose
 #   COVERAGE               - whether to use code coverage
 #   TSAN                   - whether to use thread sanitizer
-#   USE_STATIC_PLASMA     - tell ObFindLibs to favor static g-speak libraries
+#   USE_STATIC_PLASMA     - tell ObFindLibs to favor static plasma libraries
 # and sets the additional variables
 #   OPTIMIZE_FLAGS      - compiler flags for CMAKE_BUILD_TYPE
 #   COVERAGE_FLAGS      - compiler flags for COVERAGE
 #   SANITIZER_FLAGS     - compiler flags for ASAN/TSAN
 #   OB_PLASMA_LIB_TYPE - STATIC if USE_STATIC_PLASMA, else SHARED
-#                        use when calling ObFindLibs to find g-speak.
+#                        use when calling ObFindLibs to find plasma.
 #                        (Used to be named OB_PREFER_LIB_TYPE.)
 
 FUNCTION(ObCommonOpts)
@@ -31,7 +31,7 @@ FUNCTION(ObCommonOpts)
     OPTION(CMAKE_VERBOSE_MAKEFILE "Verbose output from make"           OFF)
     OPTION(COVERAGE        "Generate coverage report after test run"   OFF)
     OPTION(TSAN            "Enable Thread Sanitizer (experimental)"    OFF)
-    OPTION(USE_STATIC_PLASMA "tell ObFindLibs to favor static g-speak libraries" OFF)
+    OPTION(USE_STATIC_PLASMA "tell ObFindLibs to favor static plasma libraries" OFF)
 
     IF(DEFINED CMAKE_BUILD_TYPE AND (NOT ${CMAKE_BUILD_TYPE} STREQUAL "None"))
         # Somebody set it already, don't force it, just add the type.
