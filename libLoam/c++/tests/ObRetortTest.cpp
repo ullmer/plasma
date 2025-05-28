662b140e03c4ef9d838d58d16b1f715b6be24f3f
Brygg Ullmer <ullmer@acm.org>
2025-05-27 21:56:11 -0400

    problem characters entered

--- libLoam/c++/tests/ObRetortTest.cpp
+++ libLoam/c++/tests/ObRetortTest.cpp
@@ -342,14 +342,13 @@ TEST (ObRetortTest, RetortsInASet)
 
 #if __cplusplus >= 201103L
   // C++11: std::shuffle is available
-  std::random_device rd3;
-  std::mt19937 g3(rd3());
-  std::shuffle(vec.begin(), vec.end(), g3);
+  std::random_device rd3;
+  std::mt19937 g3(rd3());
+  std::shuffle(vec.begin(), vec.end(), g3);
 #else
   // C++98: use deprecated std::random_shuffle
   std::random_shuffle (vec.begin (), vec.end ());
 #endif
-
 
   for (std::vector<ObRetort>::iterator it = vec.begin (); it != vec.end ();
        it++)
