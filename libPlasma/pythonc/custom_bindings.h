9d30e136b2ac35f10a89c9542302cb13b5453d29
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-06-01 17:20:42 -0400

    progress

--- libPlasma/pythonc/custom_bindings.h
+++ libPlasma/pythonc/custom_bindings.h
@@ -26,6 +26,21 @@ struct Bind_oblong_plasma_Slaw : public pybind11_weaver::EntityBase {
         handle.def("MapKeys", &oblong::plasma::Slaw::MapKeys);
         handle.def("Keys",    &oblong::plasma::Slaw::MapKeys); //more pythonic
 
+        //handle.def_static("List", pybind11::overload_cast<slaw>(&Slaw::List));
+        //handle.def_static("Map", pybind11::overload_cast<slaw, slaw>(&Slaw::Map));
+
+        //handle.def_static("List",  { return Slaw::List(s);   });
+        //handle.def_static("Map",   { return Slaw::Map(k, v); });
+        //handle.def_static("Cons", [](const Slaw &car, const Slaw &r); });
+
+        //handle.def_static("List",  { return Slaw::List(s); });
+        //handle.def_static("Map",   { return Slaw::Map(k, v); });
+        //handle.def_static("Cons",  { return Slaw::Cons(car, cdr); });
+
+        handle.def_static("List",  {return oblong::plasma::Slaw::List(s); });
+        handle.def_static("Map",   {return oblong::plasma::Slaw::Map(k, v); });
+        handle.def_static("Cons",  {return oblong::plasma::Slaw::Cons(car, cdr); });
+
         handle.def("Find", static_cast<oblong::plasma::Slaw (oblong::plasma::Slaw::*)(const oblong::plasma::Slaw &) const>
 			     (&oblong::plasma::Slaw::Find));
 
