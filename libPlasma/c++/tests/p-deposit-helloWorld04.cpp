ed47bb1067615d9a4538d7c679e6c0dc7a98a933
Brygg Ullmer <ullmer@acm.org>
2025-06-01 16:47:21 -0400

    progress

--- libPlasma/c++/tests/p-deposit-helloWorld04.cpp
+++ libPlasma/c++/tests/p-deposit-helloWorld04.cpp
@@ -1,86 +1,73 @@
 
-// C++ version of p-deposit-helloWorld2.c, as auto-ported by CoPilot
-// Deposits a protein into a pool using libPlasma++
-
 #include "libPlasma/c++/Pool.h"
 #include "libPlasma/c++/Protein.h"
+#include "libPlasma/c++/Hose.h"
 #include "libLoam/c++/Str.h"
 #include "libLoam/c++/ObRetort.h"
 
 #include <iostream>
 #include <memory>
 #include <cstring>
-#include <cstdlib>
 
 using namespace oblong::plasma;
 using namespace oblong::loam;
+using namespace std;
 
-static Slaw extract_slaw(const char *arg)
-{
-    const char *colon = strchr(arg, ':');
-    if (!colon)
-    {
-        std::cerr << "error: ingest '" << arg << "' needs a colon to separate key and value" << std::endl;
+static Slaw extract_slaw(const string &arg) {
+    size_t colon_pos = arg.find(':');
+    if (colon_pos == string::npos) {
+        cerr << "error: ingest '" << arg << "' needs a colon to separate key and value" << endl;
         exit(EXIT_FAILURE);
     }
 
-    std::string keystr(arg, colon - arg);
-    Slaw key = Slaw::Make(keystr.c_str());
+    Str keystr(arg.substr(0, colon_pos).c_str());
+    Slaw key = Slaw::FromString(keystr);
 
     Slaw value;
+    string valuestr = arg.substr(colon_pos + 1);
     char *endptr;
-    int64_t int_val = strtol(colon + 1, &endptr, 10);
-    if (*endptr == '\0')
-    {
-        value = Slaw::Make(int_val);
-    }
-    else
-    {
-        double float_val = strtod(colon + 1, &endptr);
-        if (*endptr == '\0')
-        {
-            value = Slaw::Make(float_val);
-        }
-        else
-        {
-            value = Slaw::Make(colon + 1);
+    int64 int_val = strtol(valuestr.c_str(), &endptr, 10);
+    if (*endptr == '\0') {
+        value = Slaw(int_val);
+    } else {
+        float64 float_val = strtod(valuestr.c_str(), &endptr);
+        if (*endptr == '\0') {
+            value = Slaw(float_val);
+        } else {
+            Str valstr(valuestr.c_str());
+            value = Slaw::FromString(valstr);
         }
     }
 
     return Slaw::Cons(key, value);
 }
 
-int main()
-{
-    const char *dstr = "hello";
-    const char *istr = "name:world";
-    const char *pnstr = "tcp://localhost/hello";
-
+int main() {
+    const char *pool_name = "tcp://localhost/hello";
     ObRetort ret;
-    std::unique_ptr<Pool> pool(Pool::Participate(pnstr, &ret));
-    if (!pool || ret != OB_OK)
-    {
-        std::cerr << "Failed to connect to pool: " << pnstr << std::endl;
+
+    unique_ptr<Hose> hose(Pool::Participate(pool_name, &ret));
+    if (!hose || ret != OB_OK) {
+        cerr << "Failed to connect to pool: " << pool_name << endl;
         return 1;
     }
 
-    Slaw descrips = Slaw::List(dstr);
-    Slaw ingest = extract_slaw(istr);
-    Slaw ingests = Slaw::List(ingest);
+    Slaw descrips = Slaw::List(Slaw("hello"));
+    Slaw ingests = Slaw::Map(Slaw("name"), Slaw("world"));
 
-    Protein prot = Protein::From(descrips, ingests);
-    std::cerr << "depositing in " << pnstr << std::endl;
-    prot.SpewOverview(stderr);
+    Protein prot(descrips, ingests);
 
-    ret = pool->Deposit(prot);
-    if (ret != OB_OK)
-    {
-        std::cerr << "no luck on the deposit: " << ob_error_string(ret.NumericRetort()) << std::endl;
-        return pool_cmd_retort_to_exit_code(ret);
+    if (true) { // verbose
+        cerr << "depositing in " << pool_name << endl;
+        prot.ToSlaw().Spew(stderr);
     }
 
-    pool->Withdraw();
+    ret = hose->Deposit(prot);
+    if (ret != OB_OK) {
+        cerr << "no luck on the deposit: " << ob_error_string(ret.NumericRetort()) << endl;
+        return 1;
+    }
+
+    hose->Withdraw();
     return 0;
 }
-
-/// end ///
