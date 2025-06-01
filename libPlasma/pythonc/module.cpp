b2309dce51720bb37517aa406e6b1b1727b943c8
Brygg Ullmer <ullmer@acm.org>
2025-06-01 01:46:52 -0400

    progress!

--- libPlasma/pythonc/module.cpp
+++ libPlasma/pythonc/module.cpp
@@ -22,8 +22,8 @@ void AddCustomEntities(pybind11::module_ &m, const pybind11_weaver::CustomBindin
     EntityScope top_scope2(m);
     CreateEntity<Entity_oblong_plasma_Pool>(std::move(top_scope2), registry)->Update();
 
-    //EntityScope top_scope3(m);
-    //CreateEntity<Entity_oblong_plasma_Protein>(std::move(top_scope3), registry)->Update();
+    EntityScope top_scope3(m);
+    CreateEntity<Entity_oblong_plasma_Protein>(std::move(top_scope3), registry)->Update();
 
     //EntityScope top_scope4(m);
     //CreateEntity<Entity_oblong_plasma_Slaw>(std::move(top_scope4), registry)->Update();
@@ -37,17 +37,17 @@ PYBIND11_MODULE(plasma, m) {
         .def(py::init<const char*>());
 
     // Expose Protein directly under the top-level module
-    py::class_<Protein>(m, "Protein")
-        .def(py::init<>())
-        .def(py::init<Slaw>())
-        .def(py::init<Slaw, Slaw>());
+    //py::class_<Protein>(m, "Protein")
+    //    .def(py::init<>())
+    //    .def(py::init<Slaw>())
+    //    .def(py::init<Slaw, Slaw>());
 
     // Set up the custom binding registry
     pybind11_weaver::CustomBindingRegistry registry;
 
     // Register custom bindings
     registry.SetCustomBinding<Entity_oblong_plasma_Slaw>();
-    registry.SetCustomBinding<Entity_oblong_plasma_Protein>();
+    //registry.SetCustomBinding<Entity_oblong_plasma_Protein>();
     registry.SetCustomBinding<Entity_oblong_plasma_Hose>();
     registry.SetCustomBinding<Entity_oblong_plasma_Pool>();
 
