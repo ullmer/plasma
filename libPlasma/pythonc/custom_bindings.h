cc75ccc0a8f3ffc6a2bef928278ded9aaabdc792
Brygg Ullmer <ullmer@acm.org>
2025-06-01 20:36:48 -0400

    progress

--- libPlasma/pythonc/custom_bindings.h
+++ libPlasma/pythonc/custom_bindings.h
@@ -1,4 +1,3 @@
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "Slaw.h"
@@ -31,7 +30,7 @@ struct Bind_oblong_plasma_Slaw : public pybind11_weaver::EntityBase {
         //handle.def_static("Map",  pybind11::overload_cast<slaw, slaw>(&Slaw::Map));
 
         handle.def_static("List",  [](const oblong::plasma::Slaw &s){return oblong::plasma::Slaw::List(s); });
-        handle.def_static("Array", [](const oblong::plasma::Slaw &s){return oblong::plasma::Slaw::Array(s); });
+        //handle.def_static("Array", [](const oblong::plasma::Slaw &s){return oblong::plasma::Slaw::Array(s); });
         handle.def_static("Map",   [](const oblong::plasma::Slaw &k, oblong::plasma::Slaw v){
                            return oblong::plasma::Slaw::Map(k, v); });
         handle.def_static("Cons",  [](const oblong::plasma::Slaw &car, const oblong::plasma::Slaw &cdr){
