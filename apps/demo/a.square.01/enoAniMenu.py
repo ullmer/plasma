6f7a1e9d6b11228fec64c06254cbec924558ce7b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-06-02 23:08:07 -0400

    progress

--- apps/demo/a.square.01/enoAniMenu.py
+++ apps/demo/a.square.01/enoAniMenu.py
@@ -2,12 +2,15 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-06-02
 
-animist_menu
+class enoAniMenu:
 
-animist01a_100
-app01a_100
-data01a_100
-help01a_100
+  pathPrefix = 'animist_menu'
+  imgAn, imgAp, imgOp = 'animist01a_100', 'app01a_100',   'op01a_100'
+  imgDa, imgPr, imgTe = 'data01a_100',    'props01a_100', 'team01a_100'
+  imgHe               = 'help01a_100'
+
+  menuImgs    = [imgAn, imgAp, imgOp, imgDa, imgPr, imgTe, imgHe]
+
 map45Ber01a_100
 mapBer01a_100
 mapBos01a_100
@@ -16,8 +19,5 @@ mapDca01a_100
 mapLax01a_100
 mapMit01a_100
 mapTyo01a_100
-op01a_100
-props01a_100
-team01a_100
 
 ### end ###
