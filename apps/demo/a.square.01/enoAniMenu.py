40893be31280818a5242e3b6cf959ce5d85f9ede
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-06-02 23:19:20 -0400

    progress

--- apps/demo/a.square.01/enoAniMenu.py
+++ apps/demo/a.square.01/enoAniMenu.py
@@ -13,6 +13,9 @@ class enoAniMenu:
   menuHandles = ['an', 'ap', 'op', 'da', 'pr', 'te', 'he']
   actorDict   = None
 
+  x0, y0      = 50, 50
+  dx, dy      = 0,  75
+
   placeFns    = ['map45Ber01a_100', 'mapBer01a_100', 'mapBos01a_100',
                  'mapCeu01a_100',   'mapDca01a_100', 'mapLax01a_100', 
                  'mapMit01a_100',   'mapTyo01a_100']
@@ -22,5 +25,22 @@ class enoAniMenu:
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     self.prepActors()
+
+  ########### prepare actors ########### 
+
+  def prepActors(self):
+    self.actorDict = {}
+    x, y = self.x0, self.y0
+
+    for imgFn, actorHandle in zip(self.menuImgs, self.menuHandles):
+      imgPath = '%s/%s' % (self.pathPrefix, imgFn)
+      a = Actor(imgPath, pos=(x,y))
+      self.actorDict[actorHandle] = a
+      x += dx; y += dy
+
+  ########### draw ########### 
+
+  def draw(self, screen):
+    if self.actorDict is None:
 
 ### end ###
