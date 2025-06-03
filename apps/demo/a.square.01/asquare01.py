cf653da6ef50ff08d5a70ccd8a1013b72fcc43c5
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-02 22:15:41 -0400

    progress

--- apps/demo/a.square.01/asquare01.py
+++ apps/demo/a.square.01/asquare01.py
@@ -1,1 +1,24 @@
-hello world
+# Example visual application toward Animist
+# Brygg Ullmer, Clemson University
+# Begun 2025-06-02
+
+######## animist canvas ######## 
+
+class AnimCanvas:
+  imgSqFn, imgArtistFn = 'sspirito01h', 'pollaiolo01'
+  imgSpaceFn           = 'sspirito_extrap_cp1'
+  actorSq, actorArtist, actorSpace = [None]*3
+
+  ########### constructor ########### 
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.prepCats2Vals()
+
+########### constructor ########### 
+
+ac = AnimCanvas()
+
+def draw(): c.draw()
+
+### end ###
