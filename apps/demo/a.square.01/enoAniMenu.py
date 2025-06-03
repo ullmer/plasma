1ad31aedd680c7c32371c88cc3c4a8707df0e2fd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-06-02 23:04:50 -0400

    first steps toward animist menu

---
+++ apps/demo/a.square.01/enoAniMenu.py
@@ -0,0 +1,23 @@
+### First steps toward Animist menu ###
+# Brygg Ullmer, Clemson University
+# Begun 2025-06-02
+
+animist_menu
+
+animist01a_100
+app01a_100
+data01a_100
+help01a_100
+map45Ber01a_100
+mapBer01a_100
+mapBos01a_100
+mapCeu01a_100
+mapDca01a_100
+mapLax01a_100
+mapMit01a_100
+mapTyo01a_100
+op01a_100
+props01a_100
+team01a_100
+
+### end ###
