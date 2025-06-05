a26841c0f1e2e1b6042fa88644e71f8c72c87791
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-05 11:06:34 -0400

    progress

--- apps/demo/a.square.01/enoPgzASquare.py
+++ apps/demo/a.square.01/enoPgzASquare.py
@@ -15,8 +15,7 @@ class enoPgzASquare:
   boxSelected       = False
   flangeWidth       = 20
   flangeBoxOffset   = 100
-  #flangeBaseColor   = (128, 128, 128, 128) #alpha; pygame-targeted (!0)
-  flangeBaseColor   = (255, 128, 128, 255) #alpha; pygame-targeted (!0)
+  flangeBaseColor   = (128, 128, 128, 128) #alpha; pygame-targeted (!0)
   flangeSurfaces    = None
   flangeCoordinates = None
   lastBoxPos        = None
