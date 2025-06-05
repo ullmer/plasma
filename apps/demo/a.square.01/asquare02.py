7a5690cd150946b2612e35e998e6058feee70411
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-05 06:53:57 -0400

    successful asquare/box separation

--- apps/demo/a.square.01/asquare02.py
+++ apps/demo/a.square.01/asquare02.py
@@ -6,29 +6,25 @@ WIDTH, HEIGHT = 800, 800
 
 from enoActorScaled import *
 from enoAniMenu     import *
+from enoPgzASquare  import *
 
 ######## animist canvas ######## 
 
 class AnimCanvas:
-  imgSqFn, imgArtistFn = 'sspirito01h', 'pollaiolo01'
-  imgSpaceFn, imgTBox  = 'sspirito_extrap_cp1', 'transp_box01b'
-
-  actorSq, actorArtist, actorSpace, actorBox = [None]*4
-  boxSelected = False
+  imgArtistFn, imgSpaceFn = 'pollaiolo01', 'sspirito_extrap_cp1'
+  actorArtist, actorSpace = None, None
+  epas                    = None
 
   ########### constructor ########### 
 
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     self.prepActors()
+    self.epas = enoPgzASquare()
 
   ########### prepare actors ########### 
 
   def prepActors(self):
-    self.actorBox    = Actor(self.imgTBox,               pos=( 500, 500))
-    self.actorSq     = enoActorScaled(self.imgSqFn,      pos=(1000, 500), 
-                                                   scale=.2, alpha = 220)
-
     self.actorSpace  = enoActorScaled(self.imgSpaceFn,   pos=(530,525), 
                                                    scale=.75, alpha = 40)
 
@@ -39,27 +35,13 @@ class AnimCanvas:
 
   def draw(self, screen): 
     self.actorSpace.draw(screen)
-    self.actorBox.draw()
+    self.epas.draw(screen)
   
-  ########### on mouse down ########### 
+  ########### on mouse down, move, up ########### 
 
-  def on_mouse_down(self, pos):
-    if self.actorBox.collidepoint(pos): 
-      self.boxSelected = True
-      print("box selected"); return
-
-  ################## on_mouse_move ##################
-
-  def on_mouse_move(self, rel, buttons):
-    if self.boxSelected:
-      x1, y1 = self.actorBox.pos
-      dx, dy = rel
-      x2, y2 = x1+dx, y1+dy
-      self.actorBox.pos = (x2, y2)
-
-  ################## on_mouse_up ##################
-
-  def on_mouse_up(self): self.boxSelected = False
+  def on_mouse_down(self, pos):          self.epas.on_mouse_down(pos)
+  def on_mouse_move(self, rel, buttons): self.epas.on_mouse_move(rel, buttons)
+  def on_mouse_up(self):                 self.epas.on_mouse_up()
 
 ########### main ########### 
 
