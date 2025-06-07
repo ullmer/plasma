26acf5f4786b5ee9c418df7610db8dd7d9fca1dc
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-07 16:01:36 -0400

    again bifurcating

--- libPlasma/pythonc/module_createA.cpp
+++ libPlasma/pythonc/module_createA.cpp
@@ -8,6 +8,18 @@ create.def("v2int32", [](int a, int b) {
    v2int32 vec = {static_cast<int32>(a),
                                   static_cast<int32>(b)};
    return oblong::plasma::Slaw(vec);
+
+
+py::class_<v2int32>(m, "v2int32")
+    .def(py::init<>())
+    .def_readwrite("x", &v2int32::x)
+    .def_readwrite("y", &v2int32::y)
+    .def("__repr__",  {
+        return "<v2int32 x=" + std::to_string(v.x) + ", y=" + std::to_string(v.y) + ">";
+    });
+
+
+
 }, "Create a Slaw-wrapped v2int32 from two integers");
 
 //   oblong::plasma::v2int32 vec = {static_cast<oblong::plasma::int32>(a),
