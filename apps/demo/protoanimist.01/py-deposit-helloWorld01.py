eee75979a6d82f4220dea6a527a9e53c321db0ff
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-08 08:46:35 -0400

    replicated from ../a.square.01; which i think were replicated from my earlier libPlasma++ tests based upon jshrake-proposed hello-world

---
+++ apps/demo/protoanimist.01/py-deposit-helloWorld01.py
@@ -0,0 +1,27 @@
+
+import plasma
+
+def main():
+    pool_name = "tcp://localhost/hello"
+    hose = plasma.Pool.Participate(pool_name)
+    if hose is None:
+        print(f"Failed to connect to pool: {pool_name}")
+        return 1
+
+    try:
+        descrips = plasma.Slaw.List(plasma.Slaw("hello"))
+        ingests = plasma.Slaw.Map(plasma.Slaw("name"), plasma.Slaw("world"))
+        protein = plasma.Protein(descrips, ingests)
+
+        print(f"depositing in {pool_name}")
+        print(protein.ToSlaw().ToString())
+
+        ret = hose.Deposit(protein)
+        if ret.IsError():
+            print(f"no luck on the deposit: {ret}")
+            return 1
+    finally:
+        hose.Withdraw()
+
+if __name__ == "__main__":
+    main()
---
+++ apps/demo/protoanimist.01/py-deposit-helloWorld02.py
@@ -0,0 +1,41 @@
+# Warmup toward graphical multi-display hello world functionality
+# Brygg Ullmer, Clemson University
+# Begun 2025-06-01
+
+import plasma
+
+def main():
+  pool_name = "tcp://localhost/grObjPool" #graphical object pool
+  hose = plasma.Pool.Participate(pool_name)
+  if hose is None:
+    print(f"Failed to connect to pool: {pool_name}")
+    return 1
+
+  try:
+    descrips = plasma.Slaw.List(plasma.Slaw("sharedCanvas"))
+    objName   = "sq1"
+    objAction = "move"
+    objCoords = [10, 10]
+
+    psObjName, psObjAction    = plasma.Slaw(objName),      plasma.Slaw(objAction)
+    psObjX, psObjY            = plasma.Slaw(objCoords[0]), plasma.Slaw(objCoords[1]) # a v2int32 would be better
+    psObjUpdates              = plasma.Slaw.List()
+    for el in [psObjAction, psObjX, psObjY]: psObjUpdates.Add(el)
+
+    ingests = plasma.Slaw.Map(psObjName, psObjUpdates)
+    protein = plasma.Protein(descrips, ingests)
+
+    print(f"depositing in {pool_name}")
+    print(protein.ToSlaw().ToString())
+
+    ret = hose.Deposit(protein)
+    if ret.IsError():
+      print(f"no luck on the deposit: {ret}")
+      return 1
+  finally:
+    hose.Withdraw()
+
+if __name__ == "__main__":
+  main()
+
+### end ###
