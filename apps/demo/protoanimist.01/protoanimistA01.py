3503a9fc85c93b457b5a2b5ba4dcec0e4ce084f8
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-08 08:45:03 -0400

    protoanimist.a stub

---
+++ apps/demo/protoanimist.01/protoanimistA01.py
@@ -0,0 +1,9 @@
+# ProtoAnimist A01
+# Here, contemplated as a per-node coordinator and dispatcher which launches and manages windows,
+# and coordinates there evolution across and between multiple ensemble protoanimist nodes
+# Brygg Ullmer, Clemson University (hopefully soon with others)
+# Begun 2025-06-08
+
+
+
+### end ###
