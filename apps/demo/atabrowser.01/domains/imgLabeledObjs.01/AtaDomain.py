7e6bf7077866a37b25caf2857fe6f2e00d6a7e16
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 19:58:41 -0400

    progress

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/AtaDomain.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/AtaDomain.py
@@ -4,8 +4,21 @@
 
 import traceback
 
+########## animist tangible allomorphs domain ##########
+
 class AtaDomain:
-  def msg(self, msg):
-  def err(self, msg):
+  def msg(self, mstr: str): 
+    mstr2 = self.getClassName() + ' msg: ' + mstr; print(mstr2)
+
+  def err(self, estr: str):
+    estr2 = self.getClassName() + ' err: ' + estr; print(estr2)
+    traceback.print_exc(); 
+
+  def getClassName(self): return self.__class__.__name__
+
+########## main ##########
+if __name__ == "__main__":
+  ad = AtaDomain()
+  ad.msg("hello world")
 
 ### end ###
