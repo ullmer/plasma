6ab6164d83b802700999853d6c1ee7114dd95aa1
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 19:53:53 -0400

    stub

---
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/AtaDomain.py
@@ -0,0 +1,11 @@
+# Animist Tangible Allomorphs Domain
+# Brygg Ullmer, Clemson University
+# Begun 2025-07-09
+
+import traceback
+
+class AtaDomain:
+  def msg(self, msg):
+  def err(self, msg):
+
+### end ###
