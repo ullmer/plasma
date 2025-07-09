e62af1a9823bbd606177348ac5b76e79a1483b26
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 19:52:16 -0400

    progress

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ImgLabeledObj.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ImgLabeledObj.py
@@ -2,19 +2,31 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-07-09
 
+import os
+
 class ADImgLabeledObj(AtaDomain):
   yamlPath = '~/git/plasma/apps/demo/atabrowser.01/yaml'
   yamlFn   = 'us_nps/sc.yaml'
 
+  ########## constructor ##########
   def __init__(self): self.loadYaml()
 
   ########## loadYaml ##########
-
   def loadYaml(self):
     if self.yamlPath is None or self.yamlFn is None:
-      self.msg("
-    fn = self.yamlPath + self.yamlFn
-    
+      self.msg("loadYaml: yamlPath or yamlFn presently unassigned"); return
+
+    relPath = self.yamlPath + self.yamlFn
+    absPath = os.path.expanduser(relPath)
+    if not os.path.isfile(absPath):
+      self.msg("loadYaml: file of specified path not found: " + str(absPath))
+      return
+    else:
+      self.msg("loadYaml: file of specified path found: " + str(absPath))
+
+########## main ##########
+if __name__ == "__main__":
+  adilo = ADImgLabeledObj()
 
 #Initial key directories
 #./domains/imgLabeledObjs.01
