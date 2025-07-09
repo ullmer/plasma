f1d36546216e109b6266fafd0af3127bf6512c12
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 19:47:22 -0400

    progress

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ImgLabeledObj.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ImgLabeledObj.py
@@ -2,6 +2,20 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-07-09
 
+class ADImgLabeledObj(AtaDomain):
+  yamlPath = '~/git/plasma/apps/demo/atabrowser.01/yaml'
+  yamlFn   = 'us_nps/sc.yaml'
+
+  def __init__(self): self.loadYaml()
+
+  ########## loadYaml ##########
+
+  def loadYaml(self):
+    if self.yamlPath is None or self.yamlFn is None:
+      self.msg("
+    fn = self.yamlPath + self.yamlFn
+    
+
 #Initial key directories
 #./domains/imgLabeledObjs.01
 #./images/us_nps/meta
