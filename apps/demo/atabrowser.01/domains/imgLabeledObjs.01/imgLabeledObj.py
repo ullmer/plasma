90554c737398fbd1ae5418c243f21559ba6a5c3a
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 20:59:03 -0400

    progress

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
@@ -9,22 +9,25 @@ class ADImgLabeledObj(AtaDomain):
   yamlFn    = 'us_nps/sc.yaml'
   yamlPath1 = 'sc:nps:meta'
 
+  imgLabeledMetad = None
+
   ########## loadYaml ##########
   def loadYaml(self):
     try:
       super().loadYaml()
-      data = self.getYamlPath(self.yamlPath1)
-      if data is None: 
+      ilmd = self.imgLabeledMetad = self.getYamlPath(self.yamlPath1)
+      if ilmd is None: 
         self.msg("loadYaml: not finding anticipated data here: " + str(self.yamlPath1))
         return False
+      self.msg("loadYaml d: " + str(ilmd))
+
       return True
     except: self.err("loadYaml"); return False
 
 ########## main ##########
 if __name__ == "__main__":
   adilo = ADImgLabeledObj()
-  print("yaml:")
-  adilo.printYaml()
+  #print("yaml:"); adilo.printYaml()
 
 #Initial key directories
 #./domains/imgLabeledObjs.01
