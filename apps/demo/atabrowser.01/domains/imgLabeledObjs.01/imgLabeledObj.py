4c85e60123daf2f91d6e5c85b37c4859cfe2d863
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 20:09:59 -0400

    progress

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
@@ -2,12 +2,13 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-07-09
 
-import os
+import os, yaml
 from ataDomain import *
 
 class ADImgLabeledObj(AtaDomain):
   yamlPath = '~/git/plasma/apps/demo/atabrowser.01/yaml/'
   yamlFn   = 'us_nps/sc.yaml'
+  yamlD    = None
 
   ########## constructor ##########
   def __init__(self): self.loadYaml()
@@ -17,17 +18,26 @@ class ADImgLabeledObj(AtaDomain):
     if self.yamlPath is None or self.yamlFn is None:
       self.msg("loadYaml: yamlPath or yamlFn presently unassigned"); return
 
-    relPath = self.yamlPath + self.yamlFn
-    absPath = os.path.expanduser(relPath)
-    if not os.path.isfile(absPath):
-      self.msg("loadYaml: file of specified path not found: " + str(absPath))
-      return
-    else:
-      self.msg("loadYaml: file of specified path found: " + str(absPath))
+    try:
+      relPath = self.yamlPath + self.yamlFn
+      absPath = os.path.expanduser(relPath)
+      if not os.path.isfile(absPath):
+        self.msg("loadYaml: file of specified path not found: " + str(absPath))
+        return
+      else:
+        f = open(absPath)
+        self.yamlD = yaml.safe_load(f)
+        f.close()
+    except: self.err("exception")
+
+  ########## loadYaml ##########
+  def printYaml(self): print(self.yamlD)
 
 ########## main ##########
 if __name__ == "__main__":
   adilo = ADImgLabeledObj()
+  print("yaml:")
+  adilo.printYaml()
 
 #Initial key directories
 #./domains/imgLabeledObjs.01
