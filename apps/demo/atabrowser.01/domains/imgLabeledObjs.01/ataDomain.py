55d85189177e9a4225fa344ba306b662337f1c40
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-09 20:13:39 -0400

    shifting implementation

--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ataDomain.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/ataDomain.py
@@ -2,19 +2,48 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-07-09
 
-import traceback
+import os, traceback, yaml
 
 ########## animist tangible allomorphs domain ##########
 
 class AtaDomain:
+  yamlPath = '~/git/plasma/apps/demo/atabrowser.01/yaml/'
+  yamlFn   = None
+  yamlD    = None
+
+  ########## constructor ##########
+  def __init__(self): self.loadYaml()
+
+  ########## message ##########
   def msg(self, mstr: str): 
     mstr2 = self.getClassName() + ' msg: ' + mstr; print(mstr2)
 
+  def getClassName(self): return self.__class__.__name__
+
+  ########## error ##########
   def err(self, estr: str):
     estr2 = self.getClassName() + ' err: ' + estr; print(estr2)
     traceback.print_exc(); 
 
-  def getClassName(self): return self.__class__.__name__
+  ########## loadYaml ##########
+  def loadYaml(self):
+    if self.yamlPath is None or self.yamlFn is None:
+      self.msg("loadYaml: yamlPath or yamlFn presently unassigned"); return
+
+    try:
+      relPath = self.yamlPath + self.yamlFn
+      absPath = os.path.expanduser(relPath)
+      if not os.path.isfile(absPath):
+        self.msg("loadYaml: file of specified path not found: " + str(absPath))
+        return
+      else:
+        f = open(absPath)
+        self.yamlD = yaml.safe_load(f)
+        f.close()
+    except: self.err("exception")
+
+  ########## loadYaml ##########
+  def printYaml(self): print(self.yamlD)
 
 ########## main ##########
 if __name__ == "__main__":
--- apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
+++ apps/demo/atabrowser.01/domains/imgLabeledObjs.01/imgLabeledObj.py
@@ -6,32 +6,7 @@ import os, yaml
 from ataDomain import *
 
 class ADImgLabeledObj(AtaDomain):
-  yamlPath = '~/git/plasma/apps/demo/atabrowser.01/yaml/'
   yamlFn   = 'us_nps/sc.yaml'
-  yamlD    = None
-
-  ########## constructor ##########
-  def __init__(self): self.loadYaml()
-
-  ########## loadYaml ##########
-  def loadYaml(self):
-    if self.yamlPath is None or self.yamlFn is None:
-      self.msg("loadYaml: yamlPath or yamlFn presently unassigned"); return
-
-    try:
-      relPath = self.yamlPath + self.yamlFn
-      absPath = os.path.expanduser(relPath)
-      if not os.path.isfile(absPath):
-        self.msg("loadYaml: file of specified path not found: " + str(absPath))
-        return
-      else:
-        f = open(absPath)
-        self.yamlD = yaml.safe_load(f)
-        f.close()
-    except: self.err("exception")
-
-  ########## loadYaml ##########
-  def printYaml(self): print(self.yamlD)
 
 ########## main ##########
 if __name__ == "__main__":
