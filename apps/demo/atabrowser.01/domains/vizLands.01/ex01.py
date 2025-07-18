9f13358c9d823325e67feb60361ea97d54ea7b5f
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-17 23:00:14 -0400

    initial placement tweaks.  requires execution with python3 rather than pgzrun.

--- apps/demo/atabrowser.01/domains/vizLands.01/ex01.py
+++ apps/demo/atabrowser.01/domains/vizLands.01/ex01.py
@@ -2,7 +2,13 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-07-09
 
+import os
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
 WIDTH, HEIGHT = 1600, 1000
+
+import pgzrun
+
 from vizLandsPgz import *
 
 avlp = ADVizLandsPgz()
@@ -10,5 +16,7 @@ avlp.loadYaml()
 avlp.initPgz()
 
 def draw(): screen.clear(); avlp.draw(screen)
+
+pgzrun.go()
 
 ### end ###
