8e6ae5fbdc89fd80a62507972da98e05ff5fbd39
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-20 18:42:47 -0400

    progress

---
+++ apps/demo/atabrowser.01/domains/vizLands.01/ex04.py
@@ -0,0 +1,31 @@
+### Manage an array of labeled tranlucent blocks
+# Brygg Ullmer, Clemson University
+
+WIDTH, HEIGHT = 800, 800
+from ataLabelBlock import *
+
+alb = AtaLabelBlock()
+alb.createAlphaSurface("blk100", 100, 100, (80,80,100), 128)
+alb.placeAlphaTextSurface("box1", "blk100", 100, 100, "foo")
+alb.placeAlphaTextSurface("box2", "blk100", 150, 150, "bar")
+alb.placeAlphaTextSurface("box3", "blk100", 175, 175, "wah")
+
+def draw(): screen.clear(); alb.draw(screen)
+
+touched = None
+
+def on_mouse_down(pos): 
+  x, y = pos
+  touchedBlocks = alb.determineBlocksSurroundingPoint(x,y)
+  if len(touchedBlocks) > 0: 
+    print("touched: " + str(touchedBlocks))
+    global touched; touched = touchedBlocks
+
+def on_mouse_move(rel): 
+  dx, dy = rel
+  if touched is not None and len(touched) > 0: alb.moveBlocks(touched, dx, dy)
+
+def on_mouse_up(): global touched; touched = None
+
+### end ###
+
