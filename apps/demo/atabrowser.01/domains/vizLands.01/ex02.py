5e0f2d93ee8deb9e0aaaf338c8e363038f2dcbcc
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-20 09:52:57 -0400

    ataLabelBlock -- first test

---
+++ apps/demo/atabrowser.01/domains/vizLands.01/ex02.py
@@ -0,0 +1,16 @@
+### Manage an array of labeled tranlucent blocks
+# Brygg Ullmer, Clemson University
+
+WIDTH, HEIGHT = 800, 800
+from ataLabelBlock import *
+
+alb = AtaLabelBlock()
+alb.createAlphaSurface("blk100", 100, 100, (80,80,100), 128)
+alb.placeAlphaSurface("box1", "blk100", 100, 100)
+alb.placeAlphaSurface("box2", "blk100", 150, 150)
+alb.placeAlphaSurface("box2", "blk100", 175, 175)
+
+def draw(): alb.draw(screen)
+
+### end ###
+
