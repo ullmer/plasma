bc9712a1b0743d92fdb8f601aa99db2f798c0897
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-09-09 09:23:46 -0400

    progress

--- cmake-macos.sh
+++ cmake-macos.sh
@@ -1,1 +1,4 @@
-cmake .. -GNinja  -DOPENSSL_ROOT_DIR=/opt/local/libexec/openssl3  -DOPENSSL_INCLUDE_DIR=/opt/local/libexec/openssl3/include  -DCMAKE_CXX_STANDARD=17  -DCMAKE_EXE_LINKER_FLAGS="-L/opt/local/libexec/openssl3/lib"  -DCMAKE_SHARED_LINKER_FLAGS="-L/opt/local/libexec/openssl3/lib"   -DCMAKE_CXX_FLAGS="-std=c++17" -DCMAKE_INSTALL_PREFIX=/usr/local -DPKGCONFIG_INSTALL_DIR=/usr/local/lib/pkgconfig 
+cmake .. -GNinja  -DOPENSSL_ROOT_DIR=/opt/local/libexec/openssl3  -DOPENSSL_INCLUDE_DIR=/opt/local/libexec/openssl3/include  -DCMAKE_CXX_STANDARD=17  -DCMAKE_EXE_LINKER_FLAGS="-L/opt/local/libexec/openssl3/lib"  -DCMAKE_SHARED_LINKER_FLAGS="-L/opt/local/libexec/openssl3/lib"   -DCMAKE_CXX_FLAGS="-std=c++17" -DCMAKE_INSTALL_PREFIX=/usr/local -DPKGCONFIG_INSTALL_DIR=/usr/local/lib/pkgconfig -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_DATAROOTDIR=share
+
+
+
