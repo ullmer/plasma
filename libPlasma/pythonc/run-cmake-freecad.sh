a8f3b8881957b73331ae90c29b2175f91e745dd8
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-06-07 14:10:39 -0400

    comments

--- libPlasma/pythonc/run-cmake-freecad.sh
+++ libPlasma/pythonc/run-cmake-freecad.sh
@@ -1,5 +1,16 @@
+## Building plasma Python bindings for FreeCAD (macOS)
+
+### Key constraints
+- FreeCAD uses Python 3.11
+- headers must come from MacPorts Python
+- libpython must come from FreeCAD bundle
+
+### Build steps
+
 cmake .. -GNinja \
   -DPython3_EXECUTABLE=/Applications/FreeCAD.app/Contents/Resources/bin/python \
   -DPython3_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.11/include/python3.11 \
   -DPython3_LIBRARY=/Applications/FreeCAD.app/Contents/Resources/lib/libpython3.11.dylib \
   -DCMAKE_PREFIX_PATH="/opt/local;../../build"
+
+### end ###
