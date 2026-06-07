9ee55e4d88c065b80db610f5f8fd3ba58dd46e7b
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-06-07 14:07:58 -0400

    iterating

--- libPlasma/pythonc/run-cmake-freecad.sh
+++ libPlasma/pythonc/run-cmake-freecad.sh
@@ -1,7 +1,5 @@
 cmake .. -GNinja \
-  -DPYTHON_EXECUTABLE=/Applications/FreeCAD.app/Contents/Resources/bin/python \
-  -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.11/include/python3.11 \
-  -DPYTHON_LIBRARY=/Applications/FreeCAD.app/Contents/Resources/lib/libpython3.11.dylib \
-  -DPYBIND11_FINDPYTHON=OFF \
-  -DCMAKE_PREFIX_PATH="/opt/local;../../build" \
-  -DCMAKE_CXX_FLAGS="-I/opt/local/Library/Frameworks/Python.framework/Versions/3.11/include/python3.11"
+  -DPython3_EXECUTABLE=/Applications/FreeCAD.app/Contents/Resources/bin/python \
+  -DPython3_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.11/include/python3.11 \
+  -DPython3_LIBRARY=/Applications/FreeCAD.app/Contents/Resources/lib/libpython3.11.dylib \
+  -DCMAKE_PREFIX_PATH="/opt/local;../../build"
